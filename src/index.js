901d30d587a581fb6b313f8be600ca442cf14a53
scf4 <scf4@github.com>
2016-11-26 20:00:54 +0000

    Added option to show original locations and paths in error

--- src/index.js
+++ src/index.js
@@ -11,11 +11,14 @@ class ApolloError extends ExtendableError {
   constructor (name, {
     message,
     time_thrown = (new Date()).toISOString(),
-    data = {}
+    data = {},
+    showLocations,
+    showPath,
   }) {
     const t = (arguments[2] && arguments[2].thrown_at) || time_thrown;
     const d = Object.assign({}, data, ((arguments[2] && arguments[2].data) || {}));
-
+    const l = showLocations && (arguments[2] && arguments[2].locations);
+    const p = showPath && (arguments[2] && arguments[2].path);
     super(serializeName([
       name,
       t,
@@ -28,22 +31,28 @@ class ApolloError extends ExtendableError {
     this._humanized_message = message || '';
     this._time_thrown = t;
     this._data = d;
+    this._locations = l;
+    this._path = p;
   }
   serialize () {
     const name = this._name;
     const message = this._humanized_message;
     const time_thrown = this._time_thrown;
     const data = this._data;
+    const locations = this._locations;
+    const path = this._path;
     return {
       message,
       name,
       time_thrown,
-      data
+      data,
+      locations,
+      path,
     };
   }
 }
 
-export const createError = (name, data = { message: 'An error has occurred' }) => {
+export const createError = (name, data = { message: 'An error has occurred', showLocations, showPath }) => {
   const e = ApolloError.bind(null, name, data);
   errorMap.set(name, e);
   return e;
@@ -51,13 +60,16 @@ export const createError = (name, data = { message: 'An error has occurred' }) =
 
 export const formatError = (originalError, returnNull = false) => {
   const [ name, thrown_at, d ] = deserializeName(originalError.message);
+  const { locations, path } = originalError;
   const data = d !== undefined ? JSON.parse(d) : {};
   if (!name) return returnNull ? null : originalError;
   const CustomError = errorMap.get(name);
   if (!CustomError) return returnNull ? null : originalError;
   const error = new CustomError({
     thrown_at,
-    data
+    data,
+    locations,
+    path,
   });
   return error.serialize();
 };
